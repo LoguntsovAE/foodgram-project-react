22965c69abb0fd599b985011c672136203ff4946
LoguntsovAE <loguntsovae@yandex.ru>
2021-08-11 13:04:42 +0500

    Заполнены модели

--- backend/recipe/models.py
+++ backend/recipe/models.py
@@ -91,3 +91,104 @@ class IngredientItem(models.Model):
 
     def __str__(self):
         return self.ingredient.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=15, db_index=True)
+    value = models.CharField(max_length=20, null=True)
+    colour = models.CharField(max_length=20, null=True)
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.title
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='favorite_recipe',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        related_name='favorite_recipe',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = 'Избарнное'
+        verbose_name_plural = 'Избранные'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'recipe'],
+                name='unique_favorite'
+            )
+        ]
+
+    def __str__(self):
+        return f'user: {self.user.username}, recipe: {self.recipe.title}'
+
+
+class ShoppingList(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='user_shopping_list',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        related_name='shoping_list',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = 'Список покупок'
+        verbose_name_plural = 'Списки покупок'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'recipe'],
+                name='unique_shopinglist'
+            )
+        ]
+
+    def __str__(self):
+        return f'user: {self.user.username}, recipe: {self.recipe.title}'
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='follower',
+        on_delete=models.CASCADE,
+    )
+    author = models.ForeignKey(
+        User,
+        related_name='following',
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'],
+                name='unique_follow'
+            )
+        ]
+
+    def __str__(self):
+        return (
+            f'follower: {self.user.username}, following: {self.author.title}'
+        )
