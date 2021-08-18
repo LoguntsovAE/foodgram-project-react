41884a98b4b4493918ec557c20d9a94ba7c00e40
LoguntsovAE <loguntsovae@yandex.ru>
2021-08-18 14:41:40 +0500

    9

--- backend/users/forms.py
+++ backend/users/forms.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 
-from .models import CustomUser
+# from .models import CustomUser
 
 
 # class CreationForm(UserCreationForm):
