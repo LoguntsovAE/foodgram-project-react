78cbd36f01517e17be03f0c7991fe6e08e7c39cf
LoguntsovAE <loguntsovae@yandex.ru>
2021-08-18 14:38:50 +0500

    8

--- backend/users/forms.py
+++ backend/users/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
 
-class CreationForm(UserCreationForm):
-    class Meta:
-        model = CustomUser
-        fields = ('first_name', 'last_name', 'username', 'email')
+# class CreationForm(UserCreationForm):
+#     class Meta:
+#         model = CustomUser
+#         fields = ('first_name', 'last_name', 'username', 'email')
